0cec6ac2f092c45c7120292a998343462a202ec2
Veer Singh <veer.vs19@gmail.com>
2019-01-24 16:11:01 +0530

    Changed Firebase project name

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyCJpMJw8CW9yxvTg3zG7J3XKpwzMsO00NM",
-    authDomain: "codeduel-2019.firebaseapp.com",
-    databaseURL: "https://codeduel-2019.firebaseio.com",
-    projectId: "codeduel-2019",
-    storageBucket: "codeduel-2019.appspot.com",
-    messagingSenderId: "642160698867"
+    apiKey: "AIzaSyCoXiA6MbbDauFsR9GZeKU0QeNc1lcraU0",
+    authDomain: "wild-west-19.firebaseapp.com",
+    databaseURL: "https://wild-west-19.firebaseio.com",
+    projectId: "wild-west-19",
+    storageBucket: "",
+    messagingSenderId: "247540959705"
   },
   production: true,
   
